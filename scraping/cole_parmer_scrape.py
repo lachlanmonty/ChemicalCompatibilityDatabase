927a576f99de9a115d7ac3a962e02f7adacca73c
lachlanmonty <montylachlan@gmail.com>
2022-08-20 15:58:20 +1000

    upload use case

--- scraping/cole_parmer_scrape.py
+++ scraping/cole_parmer_scrape.py
@@ -5,6 +5,8 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
+from os.path import exists
+import glob
 
 
 url = "https://www.coleparmer.com/Chemical-Resistance"
@@ -30,17 +32,20 @@ time.sleep(5)
 
 chem_drop_down = Select(browser.find_element("id", chem_drop_down_name))
 
-dfs = []
-
 for chemical in chemicals:
+    chem_file_name = chemical.replace(">", "+")
+    chem_file_name = chem_file_name.replace("<", "-")
+    chem_file_name = chem_file_name.replace(":", "_")
 
+    if exists(f"../data/cole/{chem_file_name}.csv"):
+        continue
     chem_drop_down.select_by_value(chemical)
     # button class for changing the chemical
     button = r"/html/body/div[5]/div/main/div/div[2]/div/section/section[2]/section[1]/div/div/div[1]/form/div[2]/div[3]/div/button"
     button_select = browser.find_elements("xpath", button)
 
     button_select[0].click()
-    time.sleep(5)
+    # time.sleep(0.1)
     soup_chem = BeautifulSoup(browser.page_source, "html.parser")
 
     table = soup_chem.find("table")
@@ -57,8 +62,18 @@ for chemical in chemicals:
     df = pd.DataFrame(data)
     df = df.rename(columns={0: "material", 1: "compatibility"})
     df["chemical"] = chemical
-    dfs.append(df)
+
+    df.to_csv(f"../data/cole/{chem_file_name}.csv")
 
 
-cole_data = pd.concat(dfs)
-cole_data.to_csv("cole_data.csv")
+# combine the generated csvs
+
+filenames = glob.glob("../data/cole/*.csv")
+
+dfs = [pd.read_csv(fp) for fp in filenames]
+df = pd.concat(dfs)
+
+df = df.dropna(subset=["compatibility"])
+
+
+df.to_csv(f"../data/cole_data.csv")
